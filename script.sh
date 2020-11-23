1118aee3f0d36c7bc27afb54e348e59727293608
MAF-hub <68975444+MAF-hub@users.noreply.github.com>
2020-11-23 10:51:19 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+clear
+mkdir documents
+touch bot.txt
+touch name.txt
+touch owner.txt
+touch token.txt
+clear
+mv bot.txt documents
+mv name.txt documents
+mv token.txt documents
+mv owner.txt documents
+clear
+echo 'CREACION DE ARCHIVOS COMPLETA'
